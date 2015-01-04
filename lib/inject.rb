b5d6518dddd014133e4be9a944873203911210d8
tekhuy <huyle20@hotmail.com>
2015-01-04 21:50:40 +0000

    with a block do calculations

--- lib/inject.rb
+++ lib/inject.rb
@@ -3,7 +3,7 @@ class Array
   def my_inject(&block)
     block.call(self)
     memo = self.shift 
-    (self.count).times do
+    self.length.times do
       item = self.shift 
       memo = block.call(memo, item) 
     end
